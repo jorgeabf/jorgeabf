cd8574d6b9bc3aab71a6a8e9957f6aa05579be61
Jorge <aportuvas@outlook.es>
2022-11-07 18:30:39 +0100

    remove: blog

--- src/data.js
+++ src/data.js
@@ -2,37 +2,21 @@ import egidazu from "./images/portfolio-400/egidazu-web.webp";
 import autoescuela from "./images/portfolio-400/web-autoescuela.webp";
 import master from "./images/portfolio-400/master-inicio.webp";
 import master2 from "./images/portfolio-400/master-wpa.webp";
-import egidazuBlog from "./images/portfolio-400/egidazu-blog.webp";
-import vlogEgidazu from "./images/portfolio-400/vlog-egidazu.webp";
-import landingChess from "./images/portfolio-400/landing-chess.webp";
 
 export const proyectos = [
    {
-      href: "https://ajedrezegidazu.es",
+      href: "https://www.ajedrezeguidazu.com",
       imagen: egidazu,
       alt: "Sitio Web del Club Ajedrez Egidazu",
       proyecto: "Club de Ajedrez",
       fecha: "Diciembre 2021",
    },
+
    {
-      href: "https://blog.ajedrezegidazu.es",
-      imagen: egidazuBlog,
-      alt: "Blog del Club Ajedrez Egidazu",
-      proyecto: "Blog de Ajedrez",
-      fecha: "Mayo 2022",
-   },
-   {
-      href: "https://vlog.ajedrezegidazu.es",
-      imagen: vlogEgidazu,
-      alt: "Vlog del Club Ajedrez Egidazu",
-      proyecto: "Vlog de Ajedrez",
-      fecha: "Enero 2022",
-   },
-   {
-      href: "https://autoescuelasanprudencio.com",
+      href: "https://www.autoescuelasanprudencio.com",
       imagen: autoescuela,
-      alt: "Proyecto Autoescuela",
-      proyecto: "Autoescuela",
+      alt: "Sitio Web Autoescuela San Prudencio",
+      proyecto: "Web Autoescuela",
       fecha: "Octubre 2022",
    },
    {
@@ -48,12 +32,5 @@ export const proyectos = [
       alt: "Sitio Web Master en Medicina de Emergencia",
       proyecto: "Sitio Web Master",
       fecha: "Marzo 2022",
-   },
-   {
-      href: "https://jorgeabf.github.io/landing-chess",
-      imagen: landingChess,
-      alt: "Diseño landing page Ajedrez",
-      proyecto: "Landing Ajedrez",
-      fecha: "Junio 2022",
    },
 ];
