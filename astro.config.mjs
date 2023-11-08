932d1352923eac7d1fe3ed731097ef4e8d8f8dc1
jorgeabf <aportuvas@outlook.es>
2023-11-08 03:04:59 +0100

    add: view transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
-import { defineConfig } from "astro/config";
-import robotsTxt from "astro-robots-txt";
+import { defineConfig } from 'astro/config'
+import robotsTxt from 'astro-robots-txt'
 
 export default defineConfig({
-  site: "https://jorgearce.dev",
-  markdown: {
-    drafts: true,
-    syntaxHighlight: "prism", 
-  },
-  integrations: [robotsTxt()],
-});
+  site: 'https://jorgearce.dev',
+  integrations: [robotsTxt()]
+})
