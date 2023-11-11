3626d399a7c9cb5623c2845383070359cee01e36
jorgeabf <aportuvas@outlook.es>
2023-11-11 21:15:17 +0100

    add: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import robotsTxt from 'astro-robots-txt';
 import prefetch from "@astrojs/prefetch";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jorgearce.dev',
-  integrations: [robotsTxt(), prefetch()]
+  integrations: [robotsTxt(), prefetch(), sitemap()]
 });
