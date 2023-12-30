7fdef459b864ad8204e6b56e543c63bcf6f8267e
jorgeabf <aportuvas@outlook.es>
2023-12-30 01:50:51 +0100

    Update MenuItem and CardImage components, package.json, and astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import robotsTxt from 'astro-robots-txt';
-import prefetch from "@astrojs/prefetch";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jorgearce.dev',
-  integrations: [robotsTxt(), prefetch(), sitemap()]
+  prefetch: true,
+  integrations: [robotsTxt(), sitemap()]
 });
